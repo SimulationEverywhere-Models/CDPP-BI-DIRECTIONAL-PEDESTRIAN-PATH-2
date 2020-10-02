fbb81cfbfeae946610e1142d81bddc55f71cd996
Ben Earle <ben.earle@live.ca>
2020-10-02 13:44:11 -0400

    Add files via upload

---
+++ Pedestrian_WalkMA.bat
@@ -0,0 +1,4 @@
+cd /D "C:\eclipse\workspace\Bi-Directional_Pedestrian_Path\"
+"C:/eclipse/plugins/CD++Builder_1.1.0/internal/simuOrig.exe"  -m"Pedestrian_WalkMA.ma" -o"Pedestrian_WalkMAOUT.out" -l"Pedestrian_WalkMALOG.log" -t"00:00:50:000" 
+rem 
+pause
---
+++ Pedestrian_WalkMADRW.bat
@@ -0,0 +1,3 @@
+cd /D "C:\eclipse\workspace\Bi-Directional_Pedestrian_Path\"
+"C:/eclipse/plugins/CD++Builder_1.1.0/internal/drawlog.exe"  -m"Pedestrian_WalkMA.ma" -c"PedestrianWalk" -l"Pedestrian_WalkMALOG.log" -w"3" -p"0" -0 > Pedestrian_WalkMADRW.drw 
+pause
